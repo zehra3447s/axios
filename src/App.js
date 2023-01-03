e42f8a4e25cf6431249a25a7fdfb875d2609c973
zehra3447s <zehragmss3447@gmail.com>
2023-01-03 22:29:09 +0300

    z

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React,{useState,useEffect} from "react";
+import { Oval } from "react-loader-spinner";
 
 function App() {
+const [loading ,setLoading]=useState(false);
+  const [name,setName]=useState("");
+
+
+  useEffect(()=>{
+    setLoading(true)
+    setTimeout(()=>{
+      setLoading(false)
+    },2000)
+    axios.get("https://jsonplaceholder.typicode.com/users")
+    .then(res=>setName(res.data[0].name))
+
+  },[name])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      {
+        loading ?
+
+<Oval
+  height={80}
+  width={80}
+  color="#4fa94d"
+  wrapperStyle={{}}
+  wrapperClass=""
+  visible={true}
+  ariaLabel='oval-loading'
+  secondaryColor="#4fa94d"
+  strokeWidth={2}
+  strokeWidthSecondary={2}
+
+/>
+  :
+  <h1>{name}</h1>
+      } 
+     
+  </div>
   );
 }
-
 export default App;
